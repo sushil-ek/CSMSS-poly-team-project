da537de1266f609ab26d5414590ef957df0265d3
tejaswini-9643 <tayadetejaswinip@gmail.com>
2025-08-20 14:52:26 +0530

    changes

--- grc.php
+++ grc.php
@@ -35,7 +35,7 @@
             color: #6C757D;
         }
         .news {
-    width: 250px;
+    width: 180px;
             display: -webkit-box;
             -webkit-box-orient: vertical;
             -webkit-line-clamp: 2;
